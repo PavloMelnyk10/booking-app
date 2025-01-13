3eaec67690428241a784abcc922c39eeafca7a11
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2025-01-13 02:04:34 +0000

    Initial commit: project setup

---
+++ src/main/java/mate/academy/bookingapp/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package mate.academy.bookingapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok("Service is up and running!");
+    }
+}
